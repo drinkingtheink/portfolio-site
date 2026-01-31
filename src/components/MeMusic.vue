c415da1dfc40ec05e03a3a4dfcfe656d1919b168
drinkingtheink <jason.m.harrison@gmail.com>
2026-01-31 14:22:43 -0600

    portrait polish

--- src/components/MeMusic.vue
+++ src/components/MeMusic.vue
@@ -836,34 +836,11 @@
 </template>
 
 <script>
+import { idleShiftMixin } from '../mixins/idleShift';
+
 export default {
   name: 'MeMusic',
-  data() {
-    return {
-      shiftInterval: null
-    }
-  },
-  mounted() {
-    this.startIdleShift();
-  },
-  beforeUnmount() {
-    if (this.shiftInterval) {
-      clearInterval(this.shiftInterval);
-    }
-  },
-  methods: {
-    startIdleShift() {
-      const shift = () => {
-        const x = (Math.random() - 0.5) * 4;
-        const y = (Math.random() - 0.5) * 4;
-        if (this.$refs.portrait) {
-          this.$refs.portrait.style.transform = `translate(${x}px, ${y}px)`;
-        }
-      };
-      shift();
-      this.shiftInterval = setInterval(shift, 6000);
-    }
-  }
+  mixins: [idleShiftMixin]
 }
 </script>
 
